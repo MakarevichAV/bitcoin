aec07954786753d66e055f01cda005c7cc827d8e
MakarevichAV <makarevich.a.v@yandex.ru>
2020-09-16 20:32:52 +0300

    Updates

---
+++ precache-manifest.9863495f98c47c9f4468a5abe5fe0826.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3681735f59887025048d46bbec25ef97",
+    "url": "/bitcoin/index.html"
+  },
+  {
+    "revision": "de9e5c09a40e248b7abe",
+    "url": "/bitcoin/static/css/main.1992ee5d.chunk.css"
+  },
+  {
+    "revision": "f97d55cfbab574389581",
+    "url": "/bitcoin/static/js/2.13189761.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/bitcoin/static/js/2.13189761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9e5c09a40e248b7abe",
+    "url": "/bitcoin/static/js/main.ffcbbf94.chunk.js"
+  },
+  {
+    "revision": "d7b90004dc23b19fbf21",
+    "url": "/bitcoin/static/js/runtime-main.76343864.js"
+  },
+  {
+    "revision": "0ffd6cc4d818412ec9f17bd6a0ab6d3f",
+    "url": "/bitcoin/static/media/bitcoin.0ffd6cc4.png"
+  }
+]);
